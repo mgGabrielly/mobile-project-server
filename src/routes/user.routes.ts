a749a081e6d6559232693820411dcfc560aecbdb
mgGabrielly <ggabrielly.ggaby@gmail.com>
2024-01-17 15:18:40 -0300

    The end

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -7,10 +7,10 @@ const userRoutes: Router = express.Router();
 // userRoutes.get("/users", verifyJWT.verifyTokenUserCommon, UserController.getAllUsers);
 
 userRoutes.get("/users", UserController.getAllUsers);
-userRoutes.post('/create-user', UserController.createUser);
-userRoutes.get('/user/:id', UserController.getUserById);
-userRoutes.put('/update-user/:id', UserController.updateUser);
-userRoutes.delete('/delete-user/:id', UserController.deleteUser);
-userRoutes.put('/configuration/:id', UserController.updateData);
+userRoutes.post("/create-user", UserController.createUser);
+userRoutes.get("/user/:id", UserController.getUserById);
+userRoutes.put("/update-user/:id", UserController.updateUser);
+userRoutes.delete("/delete-user/:id", UserController.deleteUser);
+userRoutes.put("/configuration/:id", UserController.updateData);
 
 export default userRoutes;
