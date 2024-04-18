360e0cf8d35968a73966989ffed77c6a9067b8f0
Maria Gabrielly <104575546+mgGabrielly@users.noreply.github.com>
2024-04-18 20:45:36 -0300

    Delete prisma/migrations directory

--- prisma/migrations/20231116004214_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[description]` on the table `typesOfActivities` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "typesOfActivities_description_key" ON "typesOfActivities"("description");
